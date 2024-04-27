f2d39cb82afb8737d9561e6d057f418c84e2e5ad
AiramLD <airamluisdiaz629@gmail.com>
2024-04-27 19:33:39 +0100

    About view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1>This is an about page</h1>
+  <article class="container">
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are Airam and Andrew, a dynamic duo of developers hailing from
+        IES Las Galletas. Together, we bring a wealth of creativity, technical expertise, and a
+        shared passion for innovation to the table.
+      </p>
+      <p>
+        Andrew, with his keen eye for design and knack for problem-solving, is the creative force
+        behind the aesthetic appeal and functionality of our platform. His attention to detail
+        ensures that every aspect of our website is both visually stunning and user-friendly.
+      </p>
+      <p>
+        Airam, on the other hand, is the coding virtuoso who brings our visions to life with his
+        mastery of programming languages and software development techniques. His dedication to
+        writing clean, efficient code ensures that our platform runs smoothly and reliably.
+      </p>
+      <p>
+        Driven by a mutual love for literature and technology, we embarked on this journey to create
+        a space where book lovers like ourselves could come together to discover, organize, and
+        share their favorite reads. With our combined skills and unwavering determination, we've
+        built a platform that we're truly proud of.
+      </p>
+      <p>
+        But our journey doesn't end here. We're committed to continuous learning and improvement,
+        always seeking out new ways to enhance our platform and elevate the user experience. We're
+        excited to see where this adventure takes us and look forward to sharing it with you!
+      </p>
+
+      <p>
+        With our combined expertise in web development and a shared passion for reading, we strive
+        to continually improve and expand our platform, incorporating user feedback and embracing
+        new technologies to enhance the overall experience.
+      </p>
+      <p>
+        Thank you for joining us on this adventure. Together, let's explore the fascinating world of
+        books!
+      </p>
+    </div>
+  </article>
 </template>
